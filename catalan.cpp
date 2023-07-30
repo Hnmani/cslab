f18f784d8b4a258ccded27baab3624deb1267586
Harsh Neel Mani <mani.harshneel@gmail.com>
2023-07-30 16:29:04 +0000

    Added Comments

--- catalan.cpp
+++ catalan.cpp
@@ -1,24 +1,36 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-vector <int> catalan(100000,0);
 
-int c(int n){
-    if(catalan[n]) return catalan[n];
-    if(n==0){
-        return 1;
-    }
-    else{
-        int  x = 0;
-        for(int i=0;i<n;i++){
-            x += c(i) * c(n-i-1);
+vector<unsigned long long> catalan(100000, 0);
+// declared global vector catalan
+// to store the computed catalan numbers.
+
+unsigned long long c(unsigned long long n)
+{
+    if (catalan[n])
+        return catalan[n];
+    // Checks if the catalan number
+    // for n is already computed or not
+    else
+    {
+        unsigned long long x = 0;
+        for (int i = 0; i < n; i++)
+        {
+            // running a loop from
+            // 0 ... n-1 to calculate values
+
+            x += c(i) * c(n - i - 1);
+            // c(n) = c(0)(n-1) + c(1)(n-2) ... + c(n-1)(0) for n > 1;
         }
         catalan[n] = x;
         return x;
     }
 }
-int main(){
-    catalan[0]=1;
+int main()
+{
+    catalan[0] = 1;
+    // base condition for c(n) where c(0) return 1;
     int n;
-    cin >>n;
-    cout<< c(n);
+    cin >> n;
+    cout << c(n);
 }
--- studentsrecord.cpp
+++ studentsrecord.cpp
@@ -1,44 +1,75 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-bool cmp(pair<string,pair<float,string>>&a, pair<string,pair<float,string>> &b){
-    if(a.second.second<b.second.second) return true;
-    else if(a.second.second == b.second.second){
-        if(a.second.first<b.second.first) return true;
-        else if(a.second.first == b.second.first){
-            if(a.first<b.first) return true;
-            else return false;
-        }else return false;
-    }else return false;
+bool cmp(pair<string, pair<float, string>> &a, pair<string, pair<float, string>> &b)
+{
+    /*
+        This function is used for sorting the data,
+        it defines the order in which it has to be sorted;
+        first it is compared on the basis of name ( *.second.second)
+        then on the basis of cgpa (*.second.first)[only if name are same]
+        finally on the basis of roll[only if both name and cgpa are same].
+    */
+    if (a.second.second < b.second.second)
+        return true;
+    else if (a.second.second == b.second.second)
+    {
+        if (a.second.first < b.second.first)
+            return true;
+        else if (a.second.first == b.second.first)
+        {
+            if (a.first < b.first)
+                return true;
+            else
+                return false;
+        }
+        else
+            return false;
+    }
+    else
+        return false;
     return a.second.second < b.second.second;
 }
 
-int main(){
+int main()
+{
+
+    ifstream inputFile;
+    // inputFile reads from input.txt from which
+    // we extract the required data
+
+    inputFile.open("input.txt", ios::in);
+    fstream output;
+    // output is fstream object which writes the records to output.txt
+
+    output.open("output.txt", ios::out);
     int n;
-    cin >> n;
-    vector<pair<string, pair<float,string>>> data;
-    for(int i =0 ; i<n; i++){
+    inputFile >> n;
+    vector<pair<string, pair<float, string>>> data;
+    // data is a vector of pair of string and a pair of float and string
+    // [<roll,<cgpa,name>>]
+    for (int i = 0; i < n; i++)
+    {
         string roll;
         float marks;
-        string mk;
         string name;
-        getline(cin, roll,' ');
-        getline(cin, mk, ' ');
-        getline(cin, name);
-        marks = stof(mk);
-        pair<float,string> pr;
+        inputFile >> roll;
+        inputFile >> marks;
+        getline(inputFile, name);
+        pair<float, string> pr;
         pr.first = marks;
         pr.second = name;
-        pair<string,pair<float,string>> t;
+        pair<string, pair<float, string>> t;
         t.first = roll;
         t.second = pr;
         data.push_back(t);
     }
-    
-    sort(data.begin(),data.end(),cmp);
-    for(int i =0; i<n;i++){
-        pair<string,pair<float,string>> t = data[i];
-        cout<<t.second.second<<" "<<t.second.first<<" "<<t.first<<endl;
+
+    sort(data.begin(), data.end(), cmp);
+    // using sort function from STL and cmp function for sorting the data
+    for (int i = 0; i < n; i++)
+    {
+        pair<string, pair<float, string>> t = data[i];
+        output << t.second.second << " " << t.second.first << " " << t.first << endl;
     }
-    
 }
