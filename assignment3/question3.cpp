c3fbb9c54ba7e35817f20e8edfd3db14eb1d5f22
Harsh Neel Mani <mani.harshneel@gmail.com>
2023-08-11 22:27:34 +0530

    DFS

--- assignment3/question3.cpp
+++ assignment3/question3.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 #define N 6
-
 int main()
 {
     int arr[N][N] =
@@ -11,20 +10,28 @@ int main()
          {0, 0, 0, 0, 1, 1},
          {0, 0, 0, 0, 1, 1}};
     std::vector<bool> visited(N, false);
-    int provinces = 0;
     std::stack<int> s;
+    int provinces = 0;
     for (int i = 0; i < N; i++)
     {
         if (!visited[i])
         {
             provinces++;
             visited[i] = true;
-        }
-        for (int j = 0; j < 6; j++)
-        {
-            if (arr[i][j])
+            s.push(i);
+            while (!s.empty())
             {
-                visited[j] = true;
+                int x = s.top();
+                // std::cout << x << std::endl;
+                s.pop();
+                for (int j = 0; j < N; j++)
+                {
+                    if (arr[x][j] && !visited[j])
+                    {
+                        visited[j] = true;
+                        s.push(j);
+                    }
+                }
             }
         }
     }
