4295f1123c020a6de019568de80c04040a45acb4
Harsh Neel Mani <mani.harshneel@gmail.com>
2023-08-13 22:44:56 +0530

    question4 done

--- assignment3/question4.cpp
+++ assignment3/question4.cpp
@@ -1,8 +1,67 @@
-#include <bits/stdc++.h>
-#define N 7
+#include <iostream>
+#include <vector>
+
+int searchRotatedSortedArray(const std::vector<int> &nums, int target)
+{
+    int left = 0;
+    int right = nums.size() - 1;
+
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] == target)
+        {
+            return mid;
+        }
+
+        if (nums[left] <= nums[mid])
+        {
+            // Left half is sorted
+            if (nums[left] <= target && target < nums[mid])
+            {
+                // Target is in the left half
+                right = mid - 1;
+            }
+            else
+            {
+                // Target is in the right half
+                left = mid + 1;
+            }
+        }
+        else
+        {
+            // Right half is sorted
+            if (nums[mid] < target && target <= nums[right])
+            {
+                // Target is in the right half
+                left = mid + 1;
+            }
+            else
+            {
+                // Target is in the left half
+                right = mid - 1;
+            }
+        }
+    }
+
+    return -1; // Target not found
+}
 
 int main()
 {
-    int arr[N] = {4, 5, 6, 7, 0, 1, 2};
-    int target = 0;
+    std::vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
+    int target = 4;
+    int result = searchRotatedSortedArray(nums, target);
+
+    if (result != -1)
+    {
+        std::cout << "Target found at index: " << result << std::endl;
+    }
+    else
+    {
+        std::cout << "Target not found." << std::endl;
+    }
+
+    return 0;
 }
