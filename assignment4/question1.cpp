253d665397e67f8ee03451b0683361b7f9cfad22
Harsh Neel Mani <mani.harshneel@gmail.com>
2023-08-17 14:43:08 +0530

    quee1 done

--- assignment4/question1.cpp
+++ assignment4/question1.cpp
@@ -21,45 +21,49 @@ int main()
     for (int i = 0; i < e; i++)
     {
         int n1, n2;
-        std::cin >> n1, n2;
+        std::cin >> n1;
+        std::cin >> n2;
         n1 = n1 - 1;
         n2 = n2 - 1;
         arr[n1]->adj.push_back(arr[n2]);
         arr[n2]->parent = arr[n1];
     }
-    // node *root_node;
-    // for (int i = 0; i < n; i++)
-    // {
-    //     if (!arr[i]->parent)
-    //         root_node = arr[i];
-    // }
-    // int ll = 0;
-    // std::vector<std::vector<node *>> levels;
-    // levels[ll].push_back(root_node);
-    // while (true)
-    // {
-    //     std::vector<node *> nl;
-    //     for (int i = 0; i < levels[ll].size(); i++)
-    //     {
-    //         for (int j = 0; j < levels[ll][i]->adj.size(); j++)
-    //         {
-    //             nl.push_back(levels[ll][i]->adj[j]);
-    //         }
-    //     }
-    //     if (nl.size() == 0)
-    //         break;
-    //     levels.push_back(nl);
-    //     ++ll;
-    // }
-    // int odd_node = 0, even_node = 0;
-    // for (int i = 0; i < levels.size(); i++)
-    // {
-    //     if (i & 1)
-    //     {
-    //         odd_node += levels[i].size();
-    //     }
-    //     else
-    //         even_node += levels[i].size();
-    // }
-    // std::cout << (odd_node * even_node) - e;
+    node *root_node;
+    for (int i = 0; i < n; i++)
+    {
+        if (!arr[i]->parent)
+            root_node = arr[i];
+    }
+    int ll = 0;
+    std::vector<std::vector<node *>> levels;
+    std::vector<node *> t;
+    t.push_back(root_node);
+    levels.push_back(t);
+    std::cout << levels.size();
+    while (true)
+    {
+        std::vector<node *> nl;
+        for (int i = 0; i < levels[ll].size(); i++)
+        {
+            for (int j = 0; j < levels[ll][i]->adj.size(); j++)
+            {
+                nl.push_back(levels[ll][i]->adj[j]);
+            }
+        }
+        if (nl.size() == 0)
+            break;
+        levels.push_back(nl);
+        ++ll;
+    }
+    int odd_node = 0, even_node = 0;
+    for (int i = 0; i < levels.size(); i++)
+    {
+        if (i & 1)
+        {
+            odd_node += levels[i].size();
+        }
+        else
+            even_node += levels[i].size();
+    }
+    std::cout << (odd_node * even_node) - e;
 }
