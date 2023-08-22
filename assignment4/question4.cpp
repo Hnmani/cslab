bcb570a800e0a99a320fdde839243af95042b8fd
Harsh Neel Mani <mani.harshneel@gmail.com>
2023-08-22 16:58:56 +0530

    Update question4.cpp
    There were Unnecessary comments which I have removed.

--- assignment4/question4.cpp
+++ assignment4/question4.cpp
@@ -1,33 +1,3 @@
-// #include <bits/stdc++.h>
-
-// int main()
-// {
-//     int n;
-//     std::cin >> n;
-//     int arr[n];
-//     std::vector<int> btm;
-//     for (int i = 0; i < n; i++)
-//     {
-//         std::cin >> arr[i];
-//     }
-//     btm.push_back(0);
-//     for (int i = 1; i < n - 1; i++)
-//     {
-//         if (arr[i] <= arr[i - 1] && arr[i] <= arr[i + 1])
-//         {
-//             btm.push_back(i);
-//         }
-//     }
-//     btm.push_back(n - 1);
-//     int len = 0;
-//     for (int i = 1; i < btm.size(); i++)
-//     {
-//         if (len < btm[i] - btm[i - 1] + 1)
-//             len = btm[i] - btm[i - 1] + 1;
-//     }
-//     std::cout << len << std::endl;
-// }
-
 #include <bits/stdc++.h>
 using namespace std;
 
