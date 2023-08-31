0f423f9e7aaa008d41fb17ccf069512db55db3df
Harsh Neel Mani <mani.harshneel@gmail.com>
2023-08-31 11:41:51 +0530

    done

---
+++ assignment6/question1.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+    int N;
+    int K;
+    std::cin >> N;
+    std::cin >> K;
+    std::vector<int> arr(N);
+    for (int i = 0; i < N; i++)
+        std::cin >> arr[i];
+}
